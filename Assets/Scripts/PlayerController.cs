288cbb68e0d52ee6853b615b871ccbb971abb79f
David Ouyang <douyang@blizzard.com>
2016-02-24 19:37:10 -0800

    Add a game scene, with a movable player ship.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;  //deltaTime makes movement framerate agnostic.
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+	}
+}
