e38933e1d80791a66e1630e91a518394c56f29b5
David Ouyang <douyang@douyangmac-sf.local>
2016-02-26 19:39:21 -0800

    Use viewport to restrict player ship from traveling outside edges.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,21 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 5.0f;
+
+	float xMin;
+	float xMax;
+
+	public float padding = 1.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		// Set left and right edges for movement using camera viewport.
+		float distanceObjectToCamera = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftSide = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distanceObjectToCamera));
+		Vector3 rightSide = Camera.main.ViewportToWorldPoint (new Vector3 (1,0,distanceObjectToCamera));
+		xMin = leftSide.x + padding; 
+		xMax = rightSide.x - padding;
 	}
 	
 	// Update is called once per frame
@@ -17,5 +29,8 @@ public class PlayerController : MonoBehaviour {
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		float clampedX = Mathf.Clamp (transform.position.x, xMin, xMax);
+		transform.position = new Vector3 (clampedX, transform.position.y, transform.position.z);
 	}
 }
