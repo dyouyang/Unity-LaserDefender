f59f0fcd07bec0669767435af937a540b55e8289
David Ouyang <douyang@gmail.com>
2016-03-02 08:44:16 -0800

    Add damage to laser, add hp to enemy ships.
    Destroy missiles and ships on collisions.

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public float hp;
+
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		Projectile projectile = collider.gameObject.GetComponent<Projectile> ();
+		if (projectile != null) {
+			TakeDamageFrom(projectile);
+		}
+	}
+
+	void TakeDamageFrom (Projectile projectile)
+	{
+		hp -= projectile.damage;
+		if (hp <= 0) {
+			Destroy(gameObject);
+		}
+		projectile.OnHit ();
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	
+	public float damage;
+
+	public void OnHit ()
+	{
+		Destroy (gameObject);
+	}
+}
