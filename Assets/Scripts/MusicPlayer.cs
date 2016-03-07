3c7f01abaac049fdf0d167d534d6276314d92414
David Ouyang <douyang@gmail.com>
2016-03-06 18:39:17 -0800

    Add different music per scene, add space effect to menus.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
+
+	public AudioClip startMenuMusic;
+	public AudioClip gameMusic;
+	public AudioClip endMusic;
+
+	private AudioSource audioSource;
 	
 	void Start () {
 		if (instance != null && instance != this) {
@@ -11,7 +17,28 @@ public class MusicPlayer : MonoBehaviour {
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+
+			audioSource = GetComponent<AudioSource>();
+			audioSource.loop = true;
+			audioSource.clip = startMenuMusic;
+			audioSource.Play();
+
 		}
 		
+	}
+
+	void OnLevelWasLoaded(int level) {
+		Debug.Log ("Loaded level");
+
+		if (level == 0) {
+			// Start Menu
+			audioSource.clip = startMenuMusic;
+		} else if (level == 1) {
+			// Game
+			audioSource.clip = gameMusic;
+		} else if (level == 2) {
+			audioSource.clip = endMusic;
+		}
+		audioSource.Play ();
 	}
 }
