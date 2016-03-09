005f60c6594b634c53160234f4b4d6ba62c614eb
David Ouyang <douyang@gmail.com>
2016-03-09 09:44:44 -0800

    End scene announces winner.
    Depending on relative score between p1 and p2, a winner is shown in the title text.

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -7,11 +7,23 @@ public class ScoreDisplay : MonoBehaviour {
 	public Text P1ScoreText;
 	public Text P2ScoreText;
 
+	public Text winnerText;
 	// Use this for initialization
 	void Start () {
 
-		P1ScoreText.text = ScoreKeeper.scoreP1.ToString ();
-		P2ScoreText.text = ScoreKeeper.scoreP2.ToString ();
+		int P1score = ScoreKeeper.scoreP1;
+		int P2score = ScoreKeeper.scoreP2;
+
+		P1ScoreText.text = P1score.ToString ();
+		P2ScoreText.text = P2score.ToString ();
+
+		if (P1score > P2score) {
+			winnerText.text = "Player 1 Wins!";
+		} else if (P2score > P1score) {
+			winnerText.text = "Player 2 Wins!";
+		} else {
+			winnerText.text = "Equally matched!";
+		}
 
 		ScoreKeeper.ResetScore ();
 	
